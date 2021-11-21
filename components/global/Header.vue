b140c72d0e7f1e95b58538d09bb8bcb419745ddb
Brandon Dith-Berry <brandondithberry@gmail.com>
2021-11-21 17:04:36 -0600

    Header wip

--- components/global/Header.vue
+++ components/global/Header.vue
@@ -1,18 +1,67 @@
 <template>
   <header>
     <div class="container">
-      <div class="logo"><img src="" alt=""></div>
-      <div class="menu"><nav><ul><li><a href="">Home</a></li><li><a href="">Repairs</a></li><li><a href="">Lessons</a></li><li><a href="">Work</a></li><li><a href="">About</a></li><li><a href="">Contact</a></li></ul></nav></div>
-      <div class="cta"><a href="" class="cta">Get A Quote</a></div>
+      <div>
+        <NuxtLink to="/"><img :src="info.sitelogo" alt="MWF Guitars Logo" class="logo" /></NuxtLink>
+      </div>
+      <div class="menu">
+        <nav>
+          <ul>
+            <li><NuxtLink to="/">Home</NuxtLink></li>
+            <li><NuxtLink to="/repairs">Repairs</NuxtLink></li>
+            <li><NuxtLink to="/lessons">Lessons</NuxtLink></li>
+            <li><NuxtLink to="/work">Work</NuxtLink></li>
+            <li><NuxtLink to="/about">About</NuxtLink></li>
+            <li><NuxtLink to="/contact">Contact</NuxtLink></li>
+          </ul>
+        </nav>
+      </div>
+      <div><NuxtLink to="/quote" class="cta">Get A Quote</NuxtLink></div>
     </div>
   </header>
 </template>
 
 <script>
+import info from '~/content/site/info.json'
 export default {
   name: 'Header',
+
+  data() {
+    return {
+      info: info,
+    }
+  },
 }
 </script>
 
 <style lang="postcss" scoped>
+header {
+  padding: 20px;
+}
+
+.logo {
+  width: 140px;
+}
+
+div,
+nav ul {
+  display: flex;
+  align-content: center;
+  align-items: center;
+  justify-content: space-between;
+  justify-items: space-between;
+}
+
+nav li a {
+  padding: 10px;
+  margin: 10px;
+  color: black;
+}
+
+.cta {
+  display: block;
+  padding: 10px 20px;
+  background: black;
+  color: white;
+}
 </style>
